a6eacc4d2a703f5b56f5030f24d4adfdbbc82d4e
Filon08 <filatof-f@yandex.ru>
2024-11-26 18:41:57 +0500

    fix error

--- timer.js
+++ timer.js
@@ -28,10 +28,23 @@ roundBlock.oninput = () => { displayTime() }
 restBlock.oninput = () => { displayTime() }
 cyclesBlock.oninput = () => { displayTime() }
 
-
+function runStuff() {
+    console.log("start the exercise");
+}
+function endStuff() {
+    console.log("finish the exercise to rest");
+}
+function nextStuff() {
+    console.log("next exercise");
+}
 
 
 startBtn.onclick = function () {
+    a = +roundBlock.value
+    b = +restBlock.value
+    c = cyclesBlock.value
+ 
+
     let time = `${(+roundBlock.value + +restBlock.value) * cyclesBlock.value}`
     d = Number(time);
     let h = Math.floor(d / 3600);
@@ -39,16 +52,24 @@ startBtn.onclick = function () {
     let s = Math.floor(d % 3600 % 60);
     let t = `${h + ':' + m + ':' + s}`
     timerBlock.innerHTML = t
-    const timerId = setInterval(() => {
-        s <= 0 ?
-            (m--, s = 59, m <= 0 ? (h--, m = 59) : m)
-            : s--,
-            t = `${h + ':' + m + ':' + s}`
-        timerBlock.innerHTML = t, timerBlock.innerHTML = t, console.log('s: ', s);
-        if (h <= 0 && m<=0 && s<=0) { clearInterval(timerId), alert('Done')}
-}, 1000);
-stopBtn.onclick = function () {
-    clearInterval(timerId), timerBlock.innerHTML = 0
-}
+
+   
+        const timerId = setInterval(() => {
+            console.log('s: ', s);
+            (h <= 0 && m <= 0 && s <= 0) ? (clearInterval(timerId), console.log('Done')) :
+                (s <= 0 && h > 0 && m > 0) ? (h--, m--, s = 59) :
+                    (s <= 0 && h > 0 && m <= 0) ? (h--, m = 59, s = 59) :
+                        (s <= 0 && h <= 0 && m > 0) ? (h = 0, m--, s = 59) :
+                            (s <= 0 && h <= 0 && m <= 0) ? (h = 0, m--, s = 59) :
+
+                                s--, timerBlock.innerHTML = `${h + ':' + m + ':' + s}`
+
+
+        }, 1000);
+
+
+    stopBtn.onclick = function () {
+        clearInterval(timerId), timerBlock.innerHTML = 0
+    }
 }
 
