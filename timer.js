acef3aa7fc4445bdf578d446c8158532313fd20f
Filon08 <filatof-f@yandex.ru>
2024-11-26 15:42:16 +0500

    add function displaimer

--- timer.js
+++ timer.js
@@ -4,17 +4,35 @@ const timerBlock = document.querySelector('.mainTimer'),
     cyclesBlock = document.querySelector('#cy'),
     startBtn = document.querySelector('.strtBtn'),
     stopBtn = document.querySelector('.stpBtn')
+// input= document.querySelectorAll('input')
 
 
-restBlock.value = 15, cyclesBlock.value = 1, roundBlock.value = 130
+restBlock.value = 15, cyclesBlock.value = 1, roundBlock.value = 30
 
-let time = ((+roundBlock.value + +restBlock.value) * cyclesBlock.value)
-timerBlock.innerHTML = `${time} sec`
+function displayTime() {
+    let time = `${(+roundBlock.value + +restBlock.value) * cyclesBlock.value}`
+    d = Number(time);
+    var h = Math.floor(d / 3600);
+    var m = Math.floor(d % 3600 / 60);
+    var s = Math.floor(d % 3600 % 60);
+
+    var hDisplay = h > 0 ? h + 'h': "00";
+    var mDisplay = m > 0 ? m + 'm'  : "00";
+    var sDisplay = s > 0 ? s + 's': "00";
+    
+    timerBlock.innerHTML = `${hDisplay+':'+mDisplay+':'+sDisplay}`;
+}
+
+displayTime()
+roundBlock.oninput = () => { displayTime() }
+restBlock.oninput = () => { displayTime() }
+cyclesBlock.oninput = () => { displayTime() }
+
 
 
 
 startBtn.onclick = function () {
-    let time = ((+roundBlock.value + +restBlock.value) * cyclesBlock.value)
+    let time = Number(timerBlock.innerHTML)
     timerBlock.innerHTML = `${time} sec`
     const timerId = setInterval(() => {
         time <= 0 ?
