62349b03bb85b26b5f47de9bf5e3b8230e61ce90
Filon08 <filatof-f@yandex.ru>
2024-11-26 16:38:30 +0500

    fix error

--- timer.js
+++ timer.js
@@ -12,15 +12,15 @@ restBlock.value = 15, cyclesBlock.value = 1, roundBlock.value = 30
 function displayTime() {
     let time = `${(+roundBlock.value + +restBlock.value) * cyclesBlock.value}`
     d = Number(time);
-    var h = Math.floor(d / 3600);
-    var m = Math.floor(d % 3600 / 60);
-    var s = Math.floor(d % 3600 % 60);
+    let h = Math.floor(d / 3600);
+    let m = Math.floor(d % 3600 / 60);
+    let s = Math.floor(d % 3600 % 60);
 
-    var hDisplay = h > 0 ? h + 'h': "00";
-    var mDisplay = m > 0 ? m + 'm'  : "00";
-    var sDisplay = s > 0 ? s + 's': "00";
-    
-    timerBlock.innerHTML = `${hDisplay+':'+mDisplay+':'+sDisplay}`;
+    let hDisplay = h > 0 ? h + 'h' : "00";
+    let mDisplay = m > 0 ? m + 'm' : "00";
+    let sDisplay = s > 0 ? s + 's' : "00";
+
+    timerBlock.innerHTML = `${hDisplay + ':' + mDisplay + ':' + sDisplay}`;
 }
 
 displayTime()
@@ -32,16 +32,23 @@ cyclesBlock.oninput = () => { displayTime() }
 
 
 startBtn.onclick = function () {
-    let time = Number(timerBlock.innerHTML)
-    timerBlock.innerHTML = `${time} sec`
+    let time = `${(+roundBlock.value + +restBlock.value) * cyclesBlock.value}`
+    d = Number(time);
+    let h = Math.floor(d / 3600);
+    let m = Math.floor(d % 3600 / 60);
+    let s = Math.floor(d % 3600 % 60);
+    let t = `${h + ':' + m + ':' + s}`
+    timerBlock.innerHTML = t
     const timerId = setInterval(() => {
-        time <= 0 ?
-            clearInterval(timerId)
-            : time--,
-            timerBlock.innerHTML = `${time} sec`
-    }, 1000);
-    stopBtn.onclick = function () {
-        clearInterval(timerId), timerBlock.innerHTML = 0
-    }
+        s <= 0 ?
+            (m--, s = 59, m <= 0 ? (h--, m = 59) : m)
+            : s--,
+            t = `${h + ':' + m + ':' + s}`
+        timerBlock.innerHTML = t, timerBlock.innerHTML = t, console.log('s: ', s);
+        if (h <= 0 && m<=0 && s<=0) { clearInterval(timerId), alert('Done')}
+}, 1000);
+stopBtn.onclick = function () {
+    clearInterval(timerId), timerBlock.innerHTML = 0
+}
 }
 
